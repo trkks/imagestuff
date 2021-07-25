cab08aae9b8b9258b14c05441596efbddd1c7716
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2021-07-25 23:53:29 +0300

    Switched module selection from a match-list to an array

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
 use std::{env, process};
 use imagestuff;
 
+
+const MODULES: [(&str, fn(env::Args) -> Result<(), String>);4] = [
+    ("ascii"  , imagestuff::ascii::run),
+    ("lerp"   , imagestuff::lerp::run),
+    ("cursed" , imagestuff::cursed::run),
+    ("raycast", imagestuff::raycast::run),
+];
+
 fn main() {
     let mut args = env::args();
     // Skip the executable name
     args.next();
 
-    match args.next() {
-        Some(arg) => {
-            match arg.to_lowercase().as_str() {
-                "ascii"   => imagestuff::ascii::run(args),
-                "lerp"    => imagestuff::lerp::run(args),
-                "cursed"  => imagestuff::cursed::run(args),
-                "raycast" => imagestuff::raycast::run(args),
-                _         => {
-                    eprintln!("No such module");
-                    process::exit(1);
-                },
-            }
-        },
-        None => {
-            eprintln!("Please select a module");
+    if let Some(arg) = args.next() {
+        if let Some((_, function))
+            = MODULES.iter().find(|(name, _)| *name == arg.to_lowercase()) {
+            function(args)
+        } else {
+            eprintln!("No such module");
             process::exit(1);
-        },
+        }
+    } else {
+        eprintln!("Please select a module");
+        process::exit(1);
     }.unwrap_or_else(|e| {
         eprintln!("Error: {}", e);
+        process::exit(1);
     })
 }
