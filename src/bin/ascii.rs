905062d75151054288a2a69eef63da26472518dc
Juho Tarkkanen <50268629+trkks@users.noreply.github.com>
2022-05-07 23:01:37 +0300

    ascii: Parse arguments with terminal_toys::Smargs and allow defining a custom palette

--- src/bin/ascii.rs
+++ src/bin/ascii.rs
@@ -1,67 +1,85 @@
-use std::env;
-use std::path::PathBuf;
+use std::convert::TryFrom;
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
+
 use image::{Rgb, Pixel};
-use terminal_toys::ProgressBar;
+
+use terminal_toys::{ProgressBar, Smargs};
 
 use imagestuff::utils;
 
 
+const DEFAULT_PALETTE: [char; 8] = [' ', '-', '~', '=', 'o', '0', '@', '#'];
+
+
 pub struct AsciiConfig {
-    source_file: PathBuf,
+    source: PathBuf,
     width: u32,
     height: u32,
     inverted: bool,
+    palette: Vec<char>,
 }
 
-impl AsciiConfig {
-    pub fn new(mut args: env::Args) -> Result<Self, String> {
-        // Skip executable name
-        args.next();
+impl TryFrom<Smargs> for AsciiConfig {
+    type Error = Box<dyn std::error::Error>;
 
-        let source_file = match args.next() {
-            // Source file's path is saved as OS specific absolute path
-            Some(path) => PathBuf::from(&path).canonicalize()
-                                              .map_err(|e| e.to_string())?,
-            // Return a text description Error if unsufficient args
-            None       => return Err(format!("Didn't get a source file")),
-        };
-        // The width and height are ordered and optional
-        let (width, height) = match (args.next(), args.next()) {
-            (Some(w), Some(h)) => 
-                (w.parse::<u32>().map_err(|e| format!("{}: {}", e, w))?,
-                 h.parse::<u32>().map_err(|e| format!("{}: {}", e, h))?),
-            _ => (50, 50),
-        };
+    fn try_from(smargs: Smargs) -> Result<Self, Self::Error> {
+        let source   = smargs.gets(&["source",   "s"]);
+        let width    = smargs.gets(&["width",    "w"]);
+        let height   = smargs.gets(&["height",   "h"]);
+        let inverted = smargs.gets::<bool>(&["inverted", "i"]);
+        let palette  = smargs.gets::<String>(&["palette",  "p"]);
 
-        let inverted = if let Some(s) = args.next() {
-            s == "--inverted"
+        let source = source.or(smargs.first())
+            .ok()
+            .or_else(|| {
+                eprintln!("Need a source image file as first argument");
+                std::process::exit(1);
+            }).unwrap();
+
+        // The width and height are optional.
+        let width = width.or_else(
+            |e| if e.is_not_found() { Ok(50) } else { Err(e) }
+        )?;
+        let height = height.or_else(
+            |e| if e.is_not_found() { Ok(50) } else { Err(e) }
+        )?;
+
+        // TODO Could this be handled by Smargs::gets automatically (condition
+        // on type: if T == bool { .. } else { .. } )?
+        let inverted = inverted.is_ok();
+
+        let palette = if let Ok(s) = palette {
+            s.chars().collect::<Vec<char>>()
         } else {
-            false
+            DEFAULT_PALETTE.to_vec()
         };
 
-        Ok(AsciiConfig { source_file, width, height, inverted})
+        Ok(AsciiConfig { source, width, height, inverted, palette })
     }
 }
 
-pub fn main() -> Result<(), String> {
-    let config = AsciiConfig::new(env::args()).map_err(|e| e.to_string())?;
-    ascii_image(
-        config.source_file.to_str().unwrap(), 
-        config.width,
-        config.height,
-        config.inverted
-    )
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let AsciiConfig {
+        source,
+        width,
+        height,
+        inverted,
+        palette,
+    } = AsciiConfig::try_from(Smargs::from_env()?)?;
+
+    ascii_image(&source, width, height, inverted, &palette)
 }
 
 // Using ascii characters, generate a textfile representation of an image
 fn ascii_image(
-    srcfile: &str,
+    srcfile: &dyn AsRef<std::path::Path>,
     w: u32,
     h: u32,
-    inverted: bool
-) -> Result<(), String> {
+    inverted: bool,
+    palette: &[char],
+) -> Result<(), Box<dyn std::error::Error>> {
     utils::confirm_dir("ascii")?;
 
     // First open imagefile, confirming its validity
@@ -80,52 +98,74 @@ fn ascii_image(
         if i % w as usize == 0 && i != 0 {
             ascii.push('\n');
         }
-        let asciipixel = pixel_to_ascii(pixel, inverted);
+        let asciipixel = pixel_to_ascii(pixel, inverted, palette);
         // Push twice so that textfile looks more like an image in an editor
         ascii.push(asciipixel);
         ascii.push(asciipixel);
 
         progress.title(&format!("{}/{}", i+1, n));
-        progress.lap().map_err(|e| e.to_string())?;
+        progress.ulap();
     }
 
     let newfile = format!(
         "./ascii/{}.txt",
-        utils::filename(&srcfile)
-            .expect(&format!("No filename: {}", srcfile))
+        utils::filename(&srcfile).ok_or("No filename on source")?
     );
     println!("\nSaving to {}", newfile);
 
-    let mut file = File::create(newfile).map_err(|e| e.to_string())?;
+    let mut file = File::create(newfile)?;
 
     file.write_all(ascii.into_iter().collect::<String>().as_bytes())
-        .map_err(|e| e.to_string())
+        .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
 }
 
 /// Get an ascii character that looks like the brightness of the pixel.
-/// If not `inverted`, the text is white on dark background and vice versa.
-fn pixel_to_ascii(pixel: Rgb<u16>, inverted: bool) -> char {
-    const SHADES: [char; 8] = [' ', '-', '~', '=', 'o', '0', '@', '#'];
-
+/// If `inverted`, the text is black on white background and vice versa.
+fn pixel_to_ascii(
+    pixel: Rgb<u16>,
+    inverted: bool,
+    palette: &[char],
+) -> char {
+    // TODO Somehow normalize the "span" of brightness to the palette so that
+    // darker images don't just turn straight to black.
     let brightness = pixel.to_luma().0[0] as f32 / u16::MAX as f32;
-    let i = (brightness * (SHADES.len() - 1) as f32) as usize;
+    let i = (brightness * (palette.len() - 1) as f32) as usize;
 
-    SHADES[if inverted { SHADES.len() - 1 - i } else { i }]
+    palette[if inverted { palette.len() - 1 - i } else { i }]
 }
 
 #[cfg(test)]
 mod tests {
     use image::Rgb;
     use super::pixel_to_ascii;
+    use super::DEFAULT_PALETTE;
     #[test]
     fn test_pixel_to_ascii() {
         let n = u16::MAX;
-        assert_eq!(pixel_to_ascii(Rgb([n, n, n]), false),             '#');
-        assert_eq!(pixel_to_ascii(Rgb([n / 2, n / 2, n / 2]), false), '=');
-        assert_eq!(pixel_to_ascii(Rgb([0, 0, 0]), false),             ' ');
+        assert_eq!(
+            pixel_to_ascii(Rgb([n, n, n]), false, &DEFAULT_PALETTE),
+            '#'
+        );
+        assert_eq!(
+            pixel_to_ascii(Rgb([n / 2, n / 2, n / 2]), false, &DEFAULT_PALETTE),
+            '='
+        );
+        assert_eq!(
+            pixel_to_ascii(Rgb([0, 0, 0]), false, &DEFAULT_PALETTE),
+            ' '
+        );
 
-        assert_eq!(pixel_to_ascii(Rgb([n, n, n]), true),             ' ');
-        assert_eq!(pixel_to_ascii(Rgb([n / 2, n / 2, n / 2]), true), 'o');
-        assert_eq!(pixel_to_ascii(Rgb([0, 0, 0]), true),             '#');
+        assert_eq!(
+            pixel_to_ascii(Rgb([n, n, n]), true, &DEFAULT_PALETTE),
+            ' '
+        );
+        assert_eq!(
+            pixel_to_ascii(Rgb([n / 2, n / 2, n / 2]), true, &DEFAULT_PALETTE),
+            'o'
+        );
+        assert_eq!(
+            pixel_to_ascii(Rgb([0, 0, 0]), true, &DEFAULT_PALETTE),
+            '#'
+        );
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,7 @@
+use std::path;
 use std::io::Write;
+
+use terminal_toys::spinner::start_spinner;
 
 use image::io::{Reader as ImageReader};
 use image::DynamicImage;
@@ -7,13 +10,14 @@ use image::DynamicImage;
 /// Utility for loading an image from filepath into a DynamicImage
 pub fn open_decode<P>(path: P) -> Result<DynamicImage, String>
 where
-    P: AsRef<std::path::Path> + std::fmt::Debug,
+    P: AsRef<path::Path>,
 {
-    println!("Loading image {:?}", path);
-    ImageReader::open(path)
+    print!("Loading image {} ", path.as_ref().to_str().unwrap());
+    start_spinner(|| ImageReader::open(path)
         .map_err(|e| e.to_string())?
         .decode()
         .map_err(|e| e.to_string())
+    )
 }
 
 /// If dirname directory does not exists under current directory, prompt to
@@ -21,7 +25,7 @@ where
 pub fn confirm_dir(dirname: &str) -> Result<(), String> {
     let dir = dirname;
 
-    if !std::path::Path::new(&dir).exists() {
+    if !path::Path::new(&dir).exists() {
         print!("Directory `{}` not found. Create it [y/N]? ", dir);
         std::io::stdout().flush().map_err(|e| e.to_string())?;
         let mut answer = String::new();
@@ -39,9 +43,9 @@ pub fn confirm_dir(dirname: &str) -> Result<(), String> {
 /// Remove file extension and directory path from input string.
 pub fn filename<'a, T>(file: &'a T) -> Option<&'a str>
 where
-    T: std::convert::AsRef<std::ffi::OsStr>,
+    T: AsRef<path::Path>,
 {
-    std::path::Path::new(file).file_stem().and_then(|name| name.to_str())
+    file.as_ref().file_stem().and_then(|name| name.to_str())
 }
 
 pub fn degs_to_rads(degs: f32) -> f32 {
