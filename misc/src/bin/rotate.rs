cd0f4ffb7b4e48850246a95de1964b9991e80e9f
Juho <50268629+trkks@users.noreply.github.com>
2025-07-29 10:21:15 +0300

    feat: Add command to rotate images

---
+++ misc/src/bin/rotate.rs
@@ -0,0 +1,36 @@
+use terminal_toys::{smärgs, smargs::{self, List}};
+
+struct MyInput { degrees: usize, images: List<std::path::PathBuf>, }
+
+fn main() {
+    let MyInput { degrees, images } = terminal_toys::smärgs!(
+      "Rotate a bunch of image files by degrees (divisable by 90)",
+      MyInput {
+        degrees:("Degrees to rotate clockwise", vec!["deg"], smargs::Kind::Required),
+        images: ("An image file path",          vec!["f"],   smargs::Kind::List(1)),
+      },
+    )
+    .parse(std::env::args())
+    .unwrap();
+
+    for img in images.0 {
+        let thingy = utils::open_decode(&img).unwrap();
+        match (degrees / 90) % 4 {
+            1 => {
+                println!("Rotating 90 degrees.");
+                thingy.rotate90().save(img);
+            },
+            2 => {
+                println!("Rotating 180 degrees.");
+                thingy.rotate180().save(img);
+            },
+            3 => {
+                println!("Rotating 270 degrees.");
+                thingy.rotate270().save(img);
+            },
+            _ => {
+                println!("Nothing happened");
+            }
+        }
+    }
+}
